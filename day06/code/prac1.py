5b25371d809a89d8073e556aa02e553dd7161c00
zhra46 <241465144@qq.com>
2017-10-20 16:27:00 +0800

    2017-10-16

--- day06/code/prac1.py
+++
@@ -1,6 +0,0 @@
-list = [1,55,4,12,36,2,28]
-tempi=0
-for i in list:
-	if i >= tempi:
-		tempi = i
-print (tempi)
--- day10/code/exception.py
+++
@@ -1,5 +0,0 @@
-try:
-	while 1:
-		print('1')
-except KeyboardInterrupt as result:
-	print(result)
